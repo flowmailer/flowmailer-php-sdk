916e0a4bc353babfabfcf27774389314717c5365
Reyo Stallenberg <reyo@flowmailer.com>
2025-04-17 08:37:50 +0000

    PHP 8.1: #38 Expand and correct FlowStepType

--- src/Enum/FlowStepType.php
+++ src/Enum/FlowStepType.php
@@ -11,22 +11,32 @@ namespace Flowmailer\API\Enum;
 
 enum FlowStepType: string implements \JsonSerializable
 {
-    case ADD_ATTACHMENT    = 'addAttachment';
-    case ADD_HEADER        = 'addHeader';
-    case AGGREGATE         = 'aggregate';
-    case ANALYTICS         = 'analytics';
-    case ARCHIVE           = 'archive';
-    case EXTERNAL_CONTENT  = 'externalContent';
-    case EXTERNAL_DATA     = 'externalData';
-    case EXTRACTDATA       = 'extractdata';
-    case MAIL_PLUS_CONTACT = 'mailPlusContact';
-    case QAMAIL            = 'qamail';
-    case RESUBMIT_MESSAGE  = 'resubmitMessage';
-    case REWRITE_RECIPIENT = 'rewriteRecipient';
-    case SCHEDULE          = 'schedule';
-    case SET_SENDER        = 'setSender';
-    case SUBJECT           = 'subject';
-    case TEMPLATE          = 'template';
+    case ADD_ATTACHMENT         = 'addAttachment';
+    case ADD_HEADER             = 'addHeader';
+    case AGGREGATE              = 'aggregate';
+    case ANALYTICS              = 'analytics';
+    case ARCHIVE                = 'archive';
+    case CLAM_SCAN              = 'clamScan';
+    case CLICK_TRACKING         = 'clickTracking';
+    case DATA_CACH              = 'dataCach';
+    case DISCARD                = 'discard';
+    case DNS_LOOKUP             = 'dnsLookup';
+    case EXTERNAL_CONTENT       = 'externalContent';
+    case EXTERNAL_DATA          = 'externalData';
+    case EXTRACTDATA            = 'extractdata';
+    case LDAP_SEARCH            = 'ldapSearch';
+    case MAIL_PLUS_CONTACT      = 'mailPlusContact';
+    case OPEN_TRACKING          = 'openTracking';
+    case QAMAIL                 = 'qamail';
+    case RESET_MESSAGE          = 'resetMessage';
+    case RESUBMIT_MESSAGE       = 'resubmitMessage';
+    case REWRITE_RECIPIENT      = 'rewriteRecipient';
+    case SCHEDULE               = 'schedule';
+    case SELECT_SENDER_IDENTITY = 'selectSenderIdentity';
+    case SET_SENDER             = 'setSender';
+    case SUBJECT_TEMPLATE       = 'subjectTemplate';
+    case TEMPLATE               = 'template';
+    case TLS_HEADER             = 'tlsHeader';
 
     public function jsonSerialize(): string
     {
